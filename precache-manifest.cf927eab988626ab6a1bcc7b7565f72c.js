9228cb60cbee0d99219142b180e34ce13177ab76
kkudumu <kkudumu@gmail.com>
2020-08-14 15:49:52 -0700

    Updates

---
+++ precache-manifest.cf927eab988626ab6a1bcc7b7565f72c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c68f0a96892b27fdcc95445b7823044",
+    "url": "/monsters-project/index.html"
+  },
+  {
+    "revision": "b497e38a4c9103323334",
+    "url": "/monsters-project/static/css/main.dfa593da.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-project/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-project/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b497e38a4c9103323334",
+    "url": "/monsters-project/static/js/main.723bc157.chunk.js"
+  },
+  {
+    "revision": "2e513aa6a2b7922b23d7",
+    "url": "/monsters-project/static/js/runtime-main.2d96f434.js"
+  }
+]);
